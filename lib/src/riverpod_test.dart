4f827da0acfdb90fdde21024c587c3663afd1457
egonm12 <egonmeijers12@gmail.com>
2023-04-01 21:07:44 +0200

    set up project with required dependencies

---
+++ lib/src/riverpod_test.dart
@@ -0,0 +1,7 @@
+/// {@template riverpod_test}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class RiverpodTest {
+  /// {@macro riverpod_test}
+  const RiverpodTest();
+}
---
+++ test/src/riverpod_test_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:riverpod_test/riverpod_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('RiverpodTest', () {
+    test('can be instantiated', () {
+      expect(RiverpodTest(), isNotNull);
+    });
+  });
+}
